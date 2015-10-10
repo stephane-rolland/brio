cce3334e3223f42ee19553331366a99b4a59aa23
Maxime Quandalle <maxime@quandalle.com>
2015-10-10 19:28:42 +0200

    Import english lesson

--- models/card.js
+++ models/card.js
@@ -3,14 +3,18 @@ Cards = new Mongo.Collection('cards');
 Cards.attachSchema({
   text: {
     type: String,
+    optional: true,
   },
   image: {
     type: String,
+    optional: true,
   },
   sound: {
     type: String,
+    optional: true,
   },
   inLessons: {
     type: [String],
+    defaultValue: [],
   },
 });
