fcbb94167ebeb8c3669d862b8e8f52b781f735d8
Maxime Quandalle <maxime@quandalle.com>
2015-10-10 19:47:09 +0200

    Improve lesson importing

--- server/lib/config/accounts.js
+++ server/lib/config/accounts.js
@@ -27,16 +27,16 @@ Meteor.startup(function() {
   */
 
   // Add Google configuration entry
-  ServiceConfiguration.configurations.update(
-    { service: "google" },
-    { $set: {
-        clientId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        client_email: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        secret: "XXXXXXXXXXXXXXXXXXXXXXXX"
-      }
-    },
-    { upsert: true }
-  );
+  // ServiceConfiguration.configurations.update(
+  //   { service: "google" },
+  //   { $set: {
+  //       clientId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  //       client_email: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  //       secret: "XXXXXXXXXXXXXXXXXXXXXXXX"
+  //     }
+  //   },
+  //   { upsert: true }
+  // );
 
   // Add Linkedin configuration entry
   /*
--- server/lib/import-data.js
+++ server/lib/import-data.js
@@ -4,12 +4,26 @@ Meteor.startup(function() {
     return;
   }
 
+  var userId = Accounts.createUser({
+    username: 'admin',
+    email: 'admin@brio.io',
+    password: 'admin'
+  });
+
+  var lessonId = Lessons.insert({
+    title: 'English words',
+    userId: userId
+  });
+
   var lines = Assets.getText('default_lesson_english.txt').split('\n');
   _.each(lines, function(line) {
     var parts = line.split('§');
     var word = parts[0].trim();
     var definition = (parts[1] || '').trim();
-    var cardId = Cards.insert({ text: word });
+    var cardId = Cards.insert({
+      text: word,
+      inLessons: [lessonId],
+    });
 
     if (definition) {
       Comments.insert({ content: definition });
