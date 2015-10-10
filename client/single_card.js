7795267abe5311b73c29905af285ed4b6706a376
Stephane L. Rolland <stephane.rolland.soat@gmail.com>
2015-10-11 01:38:22 +0200

    call nextCard  at the start of the training

--- client/single_card.js
+++ client/single_card.js
@@ -1,5 +1,32 @@
+
+// so as to run nextCard once we are ready
+// we have set a reactive computation watching subsready, which is ok true when we have received the Cards data
+Template.singleCard.onCreated(function() {
+  this.autorun(function() {
+    if (FlowRouter.subsReady('cards')) {
+      nextCard();
+    }
+  });
+});
+
 Template.singleCard.helpers({
     currentCard: function(){
-      return Cards.findOne();
+
+      const cardId = Session.get('cardId');
+
+      return Cards.findOne(cardId);
     }
+
 });
+
+function nextCard(){
+  const cards = Cards. find().fetch();
+  const randomId = parseInt((Math.random()*cards.length),10);
+  console.log(cards);
+  console.log(randomId);
+  const cardId = cards[randomId]._id;
+  console.log(cardId);
+
+  Session.set('cardId', cardId)
+  console.log("passed");
+}
