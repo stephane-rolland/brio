c84470216fb64747c082d84a869313a0113abb5f
Camel Aissani <camel.aissani@nuageprive.fr>
2015-10-11 12:40:02 +0200

    Added message when no translation available

--- client/views/lessons.js
+++ client/views/lessons.js
@@ -4,6 +4,9 @@ Template.lessons.rendered = function () {
 Template.lessons.helpers({
   lessons: function(){
     return Lessons.find();
+  },
+  author: function(lesson) {
+  	return Meteor.users({userId: lesson.userId}).username;
   }
 });
 
