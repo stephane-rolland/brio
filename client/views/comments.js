98a09346548fdfb0ade9850ec9f8a5887b7972ea
Maxime Quandalle <maxime@quandalle.com>
2015-10-11 04:58:19 +0200

    Basic comments votes

--- client/views/comments.js
+++ client/views/comments.js
@@ -4,6 +4,27 @@ Template.cardComments.helpers({
   },
 });
 
+Template.cardComment.helpers({
+  nbVotes() {
+    let currentComment = Comments.findOne(this._id);
+    return currentComment.votes.length || 0;
+  },
+
+  userHasVoted() {
+    let currentComment = Comments.findOne(this._id);
+    return _.contains(currentComment.votes, Meteor.userId());
+  }
+});
+
+Template.cardComment.events({
+  'click .js-like'() {
+    Comments.update(this._id, {$addToSet: {votes: Meteor.userId()}});
+  },
+  'click .js-unlike'() {
+    Comments.update(this._id, {$pull: {votes: Meteor.userId()}});
+  }
+});
+
 Template.cardCommentAddForm.events({
   submit(evt, tpl) {
     evt.preventDefault();
