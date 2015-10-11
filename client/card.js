4bc6896ede1766b3a848f0372332c0e348175861
Maxime Quandalle <maxime@quandalle.com>
2015-10-11 02:19:36 +0200

    Show comments on click

--- client/card.js
+++ client/card.js
@@ -1,1 +1,15 @@
+Template.card.onCreated(function() {
+  this.showComment = new ReactiveVar(false);
+});
 
+Template.card.helpers({
+  showComments() {
+    return Template.instance().showComment.get();
+  }
+});
+
+Template.card.events({
+  'click .js-disclose-content'() {
+    Template.instance().showComment.set(true);
+  }
+})
